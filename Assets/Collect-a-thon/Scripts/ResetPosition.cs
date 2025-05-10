4b3c95ccc0c19a83791728ff7a66df2669339e2d
ANDYWU7 <wuyumingandy@gmail.com>
2025-05-10 15:40:46 -0700

    finished path 2

---
+++ Assets/Collect-a-thon/Scripts/ResetPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    public Vector3 spawnPoint;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Void") || (other.CompareTag("Acid") && CompareTag("Player")))
+        {
+            transform.position = spawnPoint;
+            transform.rotation = Quaternion.identity;
+        }
+        else if(CompareTag("Box") && other.name == "Acid_1")
+        {
+            transform.position = new Vector3 (-253, 9, 44.5f);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
